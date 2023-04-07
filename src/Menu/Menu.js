91ff1670be8813d5656078b5138de31280f46f11
Timofey Kremko <timkremko@gmail.com>
2023-04-07 13:54:25 +0200

    feature: add dinamic url dependence of user log

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 
 const Menu = ({items, active, setActive}) => {
+    const isUserLogged = localStorage.getItem('userEmail');
 
     function menu(){
         let menu = document.getElementById("menu");
@@ -16,40 +17,53 @@ const Menu = ({items, active, setActive}) => {
     }
 
   return (
-    <div id="menu" className={active ? 'menu active' : 'menu'}>
-        <div className="blur" onClick={() => setActive(false)}></div>
+    <div id="menu" className={active ? "menu active" : "menu"}>
+      <div className="blur" onClick={() => setActive(false)}></div>
 
-        <div className="menu__content">
-            <ul>
-                <li>
-                    <Link to="/tours" onClick={() => menu()}>туры</Link>
-                </li>
-                <li>
-                    <Link to="/booking" onClick={() => menu()}>забронировать</Link>
-                </li>
-                <li>
-                    <Link to="/gallery" onClick={() => menu()}>галерея</Link>
-                </li>
-                <li>
-                    <Link to="/virtual_tour" onClick={() => menu()}>о полете</Link>
-                </li>
-                <li>
-                    <Link to="/login" onClick={() => menu()}>аккаунт</Link>
-                </li>
-                <li>
-                    <Link to="/reviews" onClick={() => menu()}>отзывы</Link>
-                </li>
-                <li>
-                    <Link to="/help" onClick={() => menu()}>помощь</Link>
-                </li>
-                {/*<li>*/}
-                {/*    <Link to="/admin" onClick={() => menu()}>admin</Link>*/}
-                {/*</li>*/}
-            </ul>
-
-        </div>    
+      <div className="menu__content">
+        <ul>
+          <li>
+            <Link to="/tours" onClick={() => menu()}>
+              туры
+            </Link>
+          </li>
+          <li>
+            <Link to="/booking" onClick={() => menu()}>
+              забронировать
+            </Link>
+          </li>
+          <li>
+            <Link to="/gallery" onClick={() => menu()}>
+              галерея
+            </Link>
+          </li>
+          <li>
+            <Link to="/virtual_tour" onClick={() => menu()}>
+              о полете
+            </Link>
+          </li>
+          <li>
+            <Link to={isUserLogged !== 'undefind' ? '/account_info' : '/login'} onClick={() => menu()}>
+              аккаунт
+            </Link>
+          </li>
+          <li>
+            <Link to="/reviews" onClick={() => menu()}>
+              отзывы
+            </Link>
+          </li>
+          <li>
+            <Link to="/help" onClick={() => menu()}>
+              помощь
+            </Link>
+          </li>
+          {/*<li>*/}
+          {/*    <Link to="/admin" onClick={() => menu()}>admin</Link>*/}
+          {/*</li>*/}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Menu;
