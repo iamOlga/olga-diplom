9b505c87e4418b5fae84bbcd21fe04c72102937d
Timofey Kremko <timkremko@gmail.com>
2023-04-11 23:14:04 +0200

    feature: reviews account check

--- src/Reviews/Reviews.js
+++ src/Reviews/Reviews.js
@@ -2,25 +2,37 @@ import React from 'react'
 import './Reviews.css';
 import Review from "./Review";
 import Review_input from "./Review_input";
+import { Link } from 'react-router-dom'
+import Button from '../components/Button/Button';
 
 function Reviews() {
+    const isUserLogged = localStorage.getItem('userEmail')
 
     return (
-        <div className="reviews__container">
-            <h2>Отзывы</h2>
-
-            <div className="reviews">
-                <Review name="Olga Babich" date="23.05.2022" text="fvndjk jreslmdf ldjv jrfnrdkes;nv fkne lrjnvlfnvdjlnvflj jvfnldvfnvdjlvnf jlfvnldfjvnfnk"/>
-                <Review_input name="Olga Babich" text="fvndjk jreslmdf ldjv jrfnrdkes;nv fkne lrjnvlfnvdjlnvflj jvfnldvfnvdjlvnf jlfvnldfjvnfnk"/>
-            </div>
-
-            //а это будет показываться, если ты НЕ вошел в аккаунт
-            {/*<div className="link__container">*/}
-            {/*    <h3>Войдите в личный аккаунт для того, чтобы оставлять отзывы</h3>*/}
-            {/*    <Link to="/account"><Button className="button" value="Войти в аккаунт" isrow="row"/></Link>*/}
-            {/*</div>*/}
-        </div>
-    )
+      <div className="reviews__container">
+        <h2>Отзывы</h2>
+        {isUserLogged ? (
+          <div className="reviews">
+            <Review
+              name="Olga Babich"
+              date="23.05.2022"
+              text="fvndjk jreslmdf ldjv jrfnrdkes;nv fkne lrjnvlfnvdjlnvflj jvfnldvfnvdjlvnf jlfvnldfjvnfnk"
+            />
+            <Review_input
+              name="Olga Babich"
+              text="fvndjk jreslmdf ldjv jrfnrdkes;nv fkne lrjnvlfnvdjlnvflj jvfnldvfnvdjlvnf jlfvnldfjvnfnk"
+            />
+          </div>
+        ) : (
+          <div className="link__container">
+            <h3>Войдите в личный аккаунт для того, чтобы оставлять отзывы</h3>
+            <Link to="/login">
+              <Button className="button" value="Войти в аккаунт" isrow="row" />
+            </Link>
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default Reviews;
